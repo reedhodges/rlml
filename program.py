8dca2fc100f791d81d74a0d4f98e45bcfce1db64
Reed Hodges <reed.hodges@duke.edu>
2023-02-23 15:08:45 -0500

    added gradient descent

--- program.py
+++ program.py
@@ -14,17 +14,44 @@ for file_name in file_names:
 
 # win percentage
 win_percentage = []
-for i in range(0, len(data)):
-    for j in range(0, len(data[i]["teams"])):
-        win_percentage.append(data[i]["teams"][j]["cumulative"]["win_percentage"])
-
 # demos inflicted per game
 demos_inflicted_per_game = []
 for i in range(0, len(data)):
     for j in range(0, len(data[i]["teams"])):
-        demos_inflicted_per_game.append(data[i]["teams"][j]["game_average"]["demo"]["inflicted"])
+        if data[i]["teams"][j]["cumulative"]["games"] > 5:  # min 5 games played
+            win_percentage.append(data[i]["teams"][j]["cumulative"]["win_percentage"])
+            demos_inflicted_per_game.append(data[i]["teams"][j]["game_average"]["demo"]["inflicted"])
+        else:
+            pass
 
-print(len(demos_inflicted_per_game))
+# program to implement gradient descent for linear regression
+class grad_desc_LR:
+    def __init__(self, x, y, m_init, b_init, alpha, num_iter):
+        self.x = x                
+        self.y = y
+        self.m_init = m_init          # initial slope
+        self.b_init = b_init          # initial intercept
+        self.alpha = alpha          # learning rate
+        self.num_iter = num_iter    # number of iterations
+
+    def slopeintercept(self):
+        m = self.m_init
+        b = self.b_init
+        for i in range(self.num_iter):
+            # update slope and intercept using previous values, so need temporary variables
+            for j in range(len(self.x)):
+                m_tmp = m - self.alpha / len(self.x) * self.x[j] * (m * self.x[j] + b - self.y[j])
+                b_tmp = b - self.alpha / len(self.x) * (m * self.x[j] + b - self.y[j])
+                m = m_tmp
+                b = b_tmp
+        return [m, b]
+    
+fit = grad_desc_LR(demos_inflicted_per_game, win_percentage, 15, 40, 0.0001, 10000)
+slope = fit.slopeintercept()[0]
+intercept = fit.slopeintercept()[1]
+
+print("slope: " + str(slope))
+print("intercept: " + str(intercept))
 
 plt.scatter(demos_inflicted_per_game, win_percentage)
 plt.show()
