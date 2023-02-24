66e23128ff05f7f77ae3eb397065ce5deacbcd86
Reed Hodges <reed.hodges@duke.edu>
2023-02-23 22:40:02 -0500

    fixed gradient descent

--- program.py
+++ program.py
@@ -16,11 +16,14 @@ for file_name in file_names:
 win_percentage = []
 # demos inflicted per game
 demos_inflicted_per_game = []
+# boost stolen
+boost_stolen = []
 for i in range(0, len(data)):
     for j in range(0, len(data[i]["teams"])):
         if data[i]["teams"][j]["cumulative"]["games"] > 5:  # min 5 games played
             win_percentage.append(data[i]["teams"][j]["cumulative"]["win_percentage"])
             demos_inflicted_per_game.append(data[i]["teams"][j]["game_average"]["demo"]["inflicted"])
+            boost_stolen.append(data[i]["teams"][j]["game_average"]["boost"]["amount_stolen"])
         else:
             pass
 
@@ -39,19 +42,30 @@ class grad_desc_LR:
         b = self.b_init
         for i in range(self.num_iter):
             # update slope and intercept using previous values, so need temporary variables
+            # initialize sums to 0
+            [sum_m, sum_b] = [0, 0]
             for j in range(len(self.x)):
-                m_tmp = m - self.alpha / len(self.x) * self.x[j] * (m * self.x[j] + b - self.y[j])
-                b_tmp = b - self.alpha / len(self.x) * (m * self.x[j] + b - self.y[j])
-                m = m_tmp
-                b = b_tmp
+                sum_m += self.x[j] * (m * self.x[j] + b - self.y[j])
+                sum_b += m * self.x[j] + b - self.y[j]
+            # update slope and intercept using previous values, so need temporary variables
+            m_tmp = m - (self.alpha * sum_m) / len(self.x)
+            b_tmp = b - (self.alpha * sum_b) / len(self.x)
+            [m, b] = [m_tmp, b_tmp]
         return [m, b]
-    
-fit = grad_desc_LR(demos_inflicted_per_game, win_percentage, 15, 40, 0.0001, 10000)
-slope = fit.slopeintercept()[0]
-intercept = fit.slopeintercept()[1]
 
-print("slope: " + str(slope))
-print("intercept: " + str(intercept))
+# fit data to linear regression model
+# rescaled values of boost stolen to be between 0 and 100 to match win percentage
+fit = grad_desc_LR([(x-700)/(20-7) for x in boost_stolen], win_percentage, 1, 1, 0.00001, 10000)
 
-plt.scatter(demos_inflicted_per_game, win_percentage)
+# rescale the slope and intercept to get the actual values
+slope = fit.slopeintercept()[0]/(20-7)
+intercept = fit.slopeintercept()[1] - 700*slope
+
+# print values of slope and intercept
+print("slope: ", slope)
+print("intercept: ", intercept)
+
+# plot data with linear fit
+plt.scatter(boost_stolen, win_percentage, color='red')
+plt.scatter([i for i in range(500,2200,10)], [slope*i+intercept for i in range(500,2200,10)])
 plt.show()
