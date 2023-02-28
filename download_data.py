18ffccf17142b2056e6b80731b303ed819cf741d
Reed Hodges <reed.hodges@duke.edu>
2023-02-27 19:52:03 -0500

    added lines to download individual games

--- download_data.py
+++ download_data.py
@@ -2,11 +2,18 @@ import requests
 import json
 
 url = 'https://ballchasing.com/api/replays'
-
-G2_params = {'player-id':['steam:76561198293785594','steam:76561198293785594','steam:76561198994386260'], 'count':'50', 'playlist': 'private'}
 auth = {'Authorization':'TOKEN'}
 
-r = requests.get(url, params=G2_params, headers=auth)
+# JKnaps: 76561198061585314
+# Chicago: 76561198293785594
+# Atomic: 76561198994386260
+player_ids = {'player-id':['steam:76561198061585314','steam:76561198293785594','steam:76561198994386260']}
+game_count = {'count':'50'}
+playlist = {'playlist':'private'}
+
+search_params = player_ids + game_count + playlist
+
+r = requests.get(url, params=search_params, headers=auth)
 
 # save output to json file
 with open('output.json', 'w') as outfile:
--- list_urls.py
+++ list_urls.py
@@ -1,9 +1,16 @@
+import requests
 import json
 
-# import the json file
+auth = {'Authorization':'TOKEN'}
+
+# get list of replay urls
 with open('output.json') as f:
     data = json.load(f)
-
-# get all the values of the 'link' key from output.json
 urls = [data["list"][j]["link"] for j in range(len(data["list"]))]
-print(urls)
+ids = [data["list"][j]["id"] for j in range(len(data["list"]))]
+
+# download replay data
+for i in range(len(urls)):
+    r = requests.get(urls[i], headers=auth)
+    with open(str(ids[i]) + '.json', 'w') as outfile:
+        json.dump(r.json(), outfile)
